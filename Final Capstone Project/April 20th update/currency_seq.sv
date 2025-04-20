1ef5233df648717629c9002c704f5dacfe9f0e7b
Dhanush H <167459628+H-Dhanush@users.noreply.github.com>
2025-04-20 15:48:20 +0530

    Update currency_seq.sv

--- Final Capstone Project/April 20th update/currency_seq.sv
+++ Final Capstone Project/April 20th update/currency_seq.sv
@@ -1,1 +1,22 @@
+/////////////////currency_sequencer//////////////////////
 
+class currency_gen extends uvm_sequence#(currency_trans);
+  `uvm_object_utils(currency_gen)
+  currency_trans#(100) tg;
+  
+  function new(string name="currency_gen");
+    super.new(name);
+  endfunction
+  
+  virtual task body();
+    tg=currency_trans#(100)::type_id::create("tg");
+    
+    repeat(10) begin
+      start_item(tg);
+      assert(tg.randomize());
+      `uvm_info("CURRENCY_SEQ",$sformatf("currency_select_valid:%b || currency_select_val:%d " ,tg.currency_select_valid,tg.currency_value),UVM_NONE)
+      finish_item(tg);
+    end
+  endtask
+  
+endclass
