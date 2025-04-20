c214ba99acb0f1aff254e02d959f7e3facd8a2b0
Dhanush H <167459628+H-Dhanush@users.noreply.github.com>
2025-04-20 12:07:24 +0530

    Update item_disp_trans.sv

--- Final Capstone Project/April 20th update/item_disp_trans.sv
+++ Final Capstone Project/April 20th update/item_disp_trans.sv
@@ -1,1 +1,19 @@
+/////////////////////////item_disp_trans Class////////////////////////////////
+ 
+//parameterization for max items as per design file as 32 items
 
+class item_disp_trans#(parameter int MAX_ITEMS=32) extends uvm_sequence_item;
+   // factory registration
+   `uvm_object_utils(item_disp_trans)
+  
+  bit item_dispense_valid;
+  rand bit [$clog2(MAX_ITEMS)-1 : 0] item_dispense;
+  bit [15:0] currency_change; 
+
+  // construction item_disp_trans 
+  
+   function new(string name="item_disp_trans");
+    super.new(name);
+  endfunction
+  
+endclass
