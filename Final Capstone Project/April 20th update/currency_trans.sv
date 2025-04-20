a99aa1f7ba0efc8b5e24d04dcfc01d4910f4128f
Dhanush H <167459628+H-Dhanush@users.noreply.github.com>
2025-04-20 15:47:06 +0530

    Update currency_trans.sv

--- Final Capstone Project/April 20th update/currency_trans.sv
+++ Final Capstone Project/April 20th update/currency_trans.sv
@@ -1,1 +1,13 @@
+//////////////////////currency_agent//////////////////
 
+class currency_trans#(parameter int MAX_NOTE_VAL=100) extends uvm_sequence_item;
+  `uvm_object_utils(currency_trans)
+  
+  bit currency_select_valid;
+  rand bit [$clog2(MAX_NOTE_VAL)-1  : 0] currency_value;
+  
+  function new(string name="currency_trans");
+    super.new(name);
+  endfunction
+
+endclass
